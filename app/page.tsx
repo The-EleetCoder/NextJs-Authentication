7cf4ecc785d5d5cde28caca016ce1a80f3d83442
The-EleetCoder <jaijain1803@gmail.com>
2024-07-22 00:17:31 +0530

    added Shadcn button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <p>Hi auth!</p>
+    <div>
+      <p>Hi auth!</p>
+      <Button size="lg" variant="outline">Click me</Button>
+    </div>
   );
 }
