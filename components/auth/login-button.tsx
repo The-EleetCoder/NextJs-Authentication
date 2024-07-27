8adce93ec61e2a036481ffee581c2205231d986b
The-EleetCoder <jaijain1803@gmail.com>
2024-07-27 18:15:13 +0530

    layout file

--- components/auth/login-button.tsx
+++ components/auth/login-button.tsx
@@ -3,36 +3,36 @@
 import { useRouter } from "next/navigation"
 
 interface LoginButtonProps {
-    children: React.ReactNode,
-    mode?: "modal" | "redirect",
-    asChild?: boolean
+  children: React.ReactNode,
+  mode?: "modal" | "redirect",
+  asChild?: boolean
 }
 
 export const LoginButton = ({
-    children,
-    mode = "redirect",
-    asChild
+  children,
+  mode = "redirect",
+  asChild
 }: LoginButtonProps) => {
 
-    const router = useRouter();
+  const router = useRouter();
 
-    const onClick = () => {
-        router.push("/auth/login")
-    }
+  const onClick = () => {
+    router.push("/auth/login")
+  }
 
-    if (mode === "modal") {
-        return (
-            <span>
-                TODO : implement modal
-            </span>
-        )
-    }
-
+  if (mode === "modal") {
     return (
-        <span
-            onClick={onClick}
-            className="cursor-pointer">
-            {children}
-        </span>
+      <span>
+        TODO : implement modal
+      </span>
     )
+  }
+
+  return (
+    <span
+      onClick={onClick}
+      className="cursor-pointer">
+      {children}
+    </span>
+  )
 }
